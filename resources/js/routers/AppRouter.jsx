8dd4f4cc2003683117a7c33efa0f6db628f6deda
oscar lopez <oscar.lopez@alianzaelectrica.com>
2024-06-21 13:04:58 -0600

    fix(router):role_routes_validations

--- resources/js/routers/AppRouter.jsx
+++ resources/js/routers/AppRouter.jsx
@@ -4,8 +4,11 @@ import { AuthRoutes } from '../auth/routes/AuthRoutes';
 import { AdminRoutes } from '../admin/routes/AdminRoutes';
 import { HomePage } from '../client/pages/HomePage';
 import { AuthChecker } from '../auth/components';
+import { useAuthUserStore } from '../store/authUser';
 
 export const AppRouter = () => {
+  const user = useAuthUserStore(state => state.user);
+
   return (
     <>
       <AuthChecker />
@@ -19,23 +22,25 @@ export const AppRouter = () => {
           }
         />
 
-        <Route
-          path='/admin/*'
-          element={
-            <PrivateRouter>
-              <AdminRoutes />
-            </PrivateRouter>
-          }
-        />
-
-        <Route
-          path='/client/*'
-          element={
-            <PrivateRouter>
-              <HomePage />
-            </PrivateRouter>
-          }
-        />
+        {user !== null && user.role_id === 1 ? (
+          <Route
+            path='/admin/*'
+            element={
+              <PrivateRouter>
+                <AdminRoutes />
+              </PrivateRouter>
+            }
+          />
+        ) : (
+          <Route
+            path='/client/*'
+            element={
+              <PrivateRouter>
+                <HomePage />
+              </PrivateRouter>
+            }
+          />
+        )}
       </Routes>
     </>
   );
