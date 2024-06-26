cdfce8fcd1fc88ec9735de0706aa6e29dd44137c
oscar lopez <oscar.lopez@alianzaelectrica.com>
2024-06-26 16:30:56 -0600

    feat(admin_table):add_empty_msg

--- resources/js/admin/components/table/AdminTable.jsx
+++ resources/js/admin/components/table/AdminTable.jsx
@@ -19,6 +19,7 @@ export const AdminTable = ({
         tableStyle={{ minWidth: '50rem' }}
         filters={filters}
         filterDisplay='row'
+        emptyMessage='No existe ningun registro'
       >
         {tableColumns.map(colum => (
           <Column
