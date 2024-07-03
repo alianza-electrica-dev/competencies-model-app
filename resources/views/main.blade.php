d344b25d57a9ec717309a6e5879d18bbfe38e868
oscar lopez <oscar.lopez@alianzaelectrica.com>
2024-07-03 09:39:40 -0600

    feat(app):fav_icon

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="icon" href="{{ asset('assets/icons/Grupo-FG-color.svg') }}" type="image/x-icon"/>
     <title>Competencies App</title>
     @viteReactRefresh
     @vite('resources/js/main.jsx')
