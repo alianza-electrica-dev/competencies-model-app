d087bce57a338a3780e714f27c6e33c428cd3ea1
GermmainVargas <francisco.vargas@alianzaelectrica.com>
2025-04-08 18:24:55 -0600

    Components are exported

--- resources/js/admin/pages/index.js
+++ resources/js/admin/pages/index.js
@@ -1,4 +1,5 @@
 export { EmployeesCompetenciesPage } from './employees/competencies/EmployeesCompetenciesPage';
+export { RiosCompetenciesPage } from './rios/competencies/RiosCompetenciesPage';
 export { EmployeesPage } from './employees/EmployeesPage';
 export { ManagersPage } from './managers/ManagersPage';
 export { RiosPage } from './rios/RiosPage';
